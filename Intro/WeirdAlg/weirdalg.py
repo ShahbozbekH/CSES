3b2604a67bf7aed49fae81417b34b8f5df6a4b26
ShahbozbekH <hakimovshahbozbek@gmail.com>
2026-03-05 12:31:33 -0500

    weird alg solved

--- Intro/WeirdAlg/weirdalg.py
+++ Intro/WeirdAlg/weirdalg.py
@@ -1,7 +1,16 @@
 import sys
 
 if __name__ == "__main__":
-	inFile = sys.argv[1]
-	f = open(inFile)
-	fread = int(f.read(1))
-
+	#inFile = sys.argv[1]
+	#f = open(inFile)
+	#fread = int(f.readline())
+	fread = int(input())
+	ans = []
+	while (fread != 1):
+		ans.append(fread)
+		if (fread%2 == 0):
+			fread = int(fread/2)
+			continue
+		fread = (fread*3)+1
+	ans.append(1)
+	print(*ans)
