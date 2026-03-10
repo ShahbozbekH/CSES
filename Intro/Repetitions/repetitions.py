102c5516423629c7a9d38082767b97791b07a5eb
ShahbozbekH <hakimovshahbozbek@gmail.com>
2026-03-10 16:48:44 -0400

    repetitions solved

--- Intro/Repetitions/repetitions.py
+++ Intro/Repetitions/repetitions.py
@@ -1,7 +1,27 @@
-import sys
+#import sys
 
 if __name__ == "__main__":
-	inFile = sys.argv[1]
-	f = open(inFile)
-	fread = f.readline()
-
+	#inFile = sys.argv[1]
+	#f = open(inFile)
+	fread = input()
+	largest = 0
+	curr = {
+		"letter" : None,
+		"size" : 0,
+	}
+	for i in fread:
+		if curr["letter"] is None:
+			curr["letter"] = i
+			curr["size"] = 1
+			largest = 1
+			continue
+		if curr["letter"] is i:
+			curr["size"] += 1
+			if curr["size"] > largest:
+				largest = curr["size"]
+			continue
+		if curr["size"] > largest:
+			largest = curr["size"]
+		curr["letter"] = i
+		curr["size"] = 1
+	print(largest)
